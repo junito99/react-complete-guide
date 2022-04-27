e5cfe16953eaa68b6b4b8873d2985fe8c28ec790
junito99 <anjnito99@gmail.com>
2022-04-27 06:36:12 -0400

    Expenses.js added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 //import TestComma from './components/Test';
 function App() {
   const expenses = [
@@ -26,26 +26,9 @@ function App() {
     <div>
       <div>
         <h2>Let's get started!</h2>
-        <ExpenseItem
-          title={expenses[0].title}
-          date={expenses[0].date}
-          amount={expenses[0].amount}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={expenses[1].title}
-          date={expenses[1].date}
-          amount={expenses[1].amount}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={expenses[2].title}
-          date={expenses[2].date}
-          amount={expenses[2].amount}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={expenses[3].title}
-          date={expenses[3].date}
-          amount={expenses[3].amount}
-        ></ExpenseItem>
+        <Expenses
+          items={expenses}
+        />
       </div>
     </div>
   );
